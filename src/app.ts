fbb62b85599c2fa261b26e5823bcbc91c09eb6c8
Benoît Hubert <benoithubert@gmail.com>
2021-07-05 18:34:59 +0200

    (very) basic register works (in-memory)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,50 @@
 import * as express from 'express';
+import * as bcrypt from 'bcrypt';
 
 const app: express.Application = express();
 
+app.use(express.json());
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({ message: 'yo' });
+});
+
+interface RetNumber {
+  (): Number;
+}
+const genId: RetNumber = (() => {
+  let number = 0;
+  return (): Number => {
+    number += 1;
+    return number;
+  };
+})();
+
+const users = [];
+
+app.post('/register', async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
+  if (typeof email !== 'string' || email.length === 0) {
+    return res.status(400).send({ error: 'missing email' });
+  }
+  if (typeof password !== 'string' || password.length === 0) {
+    return res.status(400).send({ error: 'missing password' });
+  }
+  if (password.length < 8) {
+    return res.status(400).send({ error: 'password too short' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = {
+    id: genId(),
+    email,
+    password: hashedPassword,
+  };
+  users.push(user);
+  return res.status(201).send({
+    id: user.id,
+    email: user.email,
+  });
 });
 
 export default app;
--- test/app.spec.ts
+++ test/app.spec.ts
@@ -10,4 +10,41 @@ describe('test app routes', () => {
       const { message } = res.body;
       assert.strictEqual(message, 'yo');
     }));
+
+  it('POST / register returns user id', () => request(app)
+    .post('/register')
+    .send({ email: 'john@doe.com', password: 'john2021' })
+    .expect(201)
+    .then((res: request.Response) => {
+      const { id, email } = res.body;
+      assert.strictEqual(id, 1);
+      assert.strictEqual(email, 'john@doe.com');
+    }));
+
+  it('POST / register returns 400 (missing email)', () => request(app)
+    .post('/register')
+    .send({})
+    .expect(400)
+    .then((res: request.Response) => {
+      const { error } = res.body;
+      assert.strictEqual(error, 'missing email');
+    }));
+
+  it('POST / register returns 400 (missing password)', () => request(app)
+    .post('/register')
+    .send({ email: 'john@doe.com' })
+    .expect(400)
+    .then((res: request.Response) => {
+      const { error } = res.body;
+      assert.strictEqual(error, 'missing password');
+    }));
+
+  it('POST / register returns 400 (password too short)', () => request(app)
+    .post('/register')
+    .send({ email: 'john@doe.com', password: 'abc' })
+    .expect(400)
+    .then((res: request.Response) => {
+      const { error } = res.body;
+      assert.strictEqual(error, 'password too short');
+    }));
 });
